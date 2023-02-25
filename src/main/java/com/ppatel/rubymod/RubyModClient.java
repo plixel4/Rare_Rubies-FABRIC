92e0905128eb026b2d7fb15679ee2aff3f53c2a7
Forestry3 <111252230+Protector9@users.noreply.github.com>
2023-02-25 13:05:12 -0500

    Initial commit

---
+++ src/main/java/com/ppatel/rubymod/RubyModClient.java
@@ -0,0 +1,10 @@
+package com.ppatel.rubymod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RubyModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
